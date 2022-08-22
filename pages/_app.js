2a9c731450d2e153cc76e5e89961a82e6642bd07
Denis Poplavskii <normal64@mail.ru>
2022-08-22 13:34:19 +0200

    gh

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css'
 import Layout from "./components/Layout"
-import CurrencyStyles from "../styles/Currency.module.scss";
-import InfoapiStyles from "../styles/Infoapi.module.scss"
-import DonationStyles from "../styles/Donate.module.scss"
-import HeaderStyles from '../../styles/Header.module.scss'
+
 
 function MyApp({ Component, pageProps }) {
   return(
